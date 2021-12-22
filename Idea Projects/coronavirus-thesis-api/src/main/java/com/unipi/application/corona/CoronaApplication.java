773201382b215a4ea3769faabbed37845f5e0797
dpagidas <dim_pag@hotmail.com>
2021-12-22 10:39:00 +0200

    changes ssl

--- Idea Projects/coronavirus-thesis-api/src/main/java/com/unipi/application/corona/CoronaApplication.java
+++ Idea Projects/coronavirus-thesis-api/src/main/java/com/unipi/application/corona/CoronaApplication.java
@@ -1,10 +1,43 @@
 package com.unipi.application.corona;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CoronaApplication {
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(80);
+		connector.setSecure(false);
+		connector.setRedirectPort(443);
+		return connector;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaApplication.class, args);
--- UI Projects/coronavirus-UI/OneDrive/Υπολογιστής/corona-UI/src/environments/environment.prod.ts
+++ UI Projects/coronavirus-UI/OneDrive/Υπολογιστής/corona-UI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   staging: false,
-  baseApiUrl: 'http://167.172.100.171:8080',
+  baseApiUrl: 'https://167.172.100.171:8443',
 };
